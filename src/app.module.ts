0f776f8b67457bd775d074f10ec2f6a5d2067027
Frank Wei <frank.wei@davionlabs.com>
2022-06-01 17:39:52 +0800

    refactor(db): move to `PrismaModule`

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,18 @@ import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaService } from './prisma.service';
 import { EmailModule } from './email/email.module';
 import { TelegramGroupModule } from './telegram-group/telegram-group.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
     ScheduleModule.forRoot(),
     EmailModule,
-    TelegramGroupModule
+    TelegramGroupModule,
+    PrismaModule
   ],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService],
 })
 export class AppModule {}
