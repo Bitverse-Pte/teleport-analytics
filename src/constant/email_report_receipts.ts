9be3749fddc0bbd2d520d4e32b8958899340f768
Frank Wei <frank.wei@davionlabs.com>
2022-06-15 17:10:29 +0800

    chore(email): go with their gmails

--- src/constant/email_report_receipts.ts
+++ src/constant/email_report_receipts.ts
@@ -1,6 +1,6 @@
 export const EMAIL_REPORT_RECIPIENTS = [
     "frank.wei@davionlabs.com",
-    "yana.li@davionlabs.com",
-    "kelvin.chen@davionlabs.com",
-    "era.jiang@davionlabs.com"
+    "lsyanaism@gmail.com",
+    "kelvin1224@gmail.com",
+    "era.jiang@gmail.com"
 ];
