1c2a423ee652378ae2e9f927546b82529b6ad9fc
Frank Wei <frank.wei@davionlabs.com>
2022-06-15 18:18:56 +0800

    fix(db): init data since no direct access 2 db

---
+++ prisma/migrations/20220615101442_init_data_by_migration_since_no_direct_access/migration.sql
@@ -0,0 +1,7 @@
+-- This is an init migration file in order to set listening data
+
+-- Discord Guild
+INSERT INTO DiscordGuild (id) VALUES('940126819380776970');
+
+-- Telegram Chats
+INSERT INTO TelegramGroup (chatId) VALUES(-1001109266972);
