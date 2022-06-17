3b14562ec868bc3131c36659396c0715cc2d3673
Frank Wei <frank.wei@davionlabs.com>
2022-06-17 16:13:01 +0800

    chore: run at 16;19

--- src/discord/discord.service.ts
+++ src/discord/discord.service.ts
@@ -181,7 +181,7 @@ export class DiscordService {
     }
 
     // @Cron(CronExpression.EVERY_DAY_AT_8AM, {
-    @Cron(`05 16 * * *`, {
+    @Cron(`19 16 * * *`, {
         timeZone: 'Asia/Shanghai'
     })
     private async syncMembersInChannels() {
