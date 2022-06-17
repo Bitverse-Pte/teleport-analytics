d35e68e30c4c1f197c62c48e6462485eaec404e2
Frank Wei <frank.wei@davionlabs.com>
2022-06-17 14:08:58 +0800

    chore: add uptime to '/metrics'

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import * as moment from 'moment'
 @Controller()
 export class AppController {
+  private readonly appLaunchedAt = Date.now();
   constructor(private readonly appService: AppService) {}
 
   @Get()
@@ -12,6 +13,12 @@ export class AppController {
 
   @Get('/metrics')
   iAmStillAlive() {
-    return 'Still alive';
+    const msThatLasted = Date.now() - this.appLaunchedAt;
+    const uptime = {
+      appLaunchedAt: moment(this.appLaunchedAt).toLocaleString(),
+      ms: msThatLasted,
+      timeFromNow: moment(this.appLaunchedAt).fromNow()
+    }
+    return { message: 'Still alive', uptime };
   }
 }
