670b42c7f86dc3481b5f2159d1ea34d561b899c1
Frank Wei <frank.wei@davionlabs.com>
2022-06-24 19:47:57 +0800

    fix: disable global prefix

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('/tlp_analytic_api');
+  // app.setGlobalPrefix('/tlp_analytic_api');
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app)
   await app.listen(3000);
