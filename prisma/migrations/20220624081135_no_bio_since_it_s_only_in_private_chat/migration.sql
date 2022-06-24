fa973409859f8e429f8f9cb0c28e896efc7d8704
Frank Wei <frank.wei@davionlabs.com>
2022-06-24 16:11:43 +0800

    refactor(db): no `bio` since it's only in private chat

---
+++ prisma/migrations/20220624081135_no_bio_since_it_s_only_in_private_chat/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `bio` on the `TelegramGroup` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `TelegramGroup` DROP COLUMN `bio`;
