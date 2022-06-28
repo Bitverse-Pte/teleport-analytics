4e52a37a504abdb952eb2cbc23fdd779fed855d0
Frank Wei <frank.wei@davionlabs.com>
2022-06-28 11:46:21 +0800

    fix: missing imports

--- src/fail-safe-indicator/fail-safe-indicator.module.ts
+++ src/fail-safe-indicator/fail-safe-indicator.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from 'src/prisma/prisma.module';
 import { FailSafeIndicatorService } from './fail-safe-indicator.service';
 
 @Module({
+  imports: [PrismaModule],
   providers: [FailSafeIndicatorService],
   exports: [FailSafeIndicatorService]
 })
